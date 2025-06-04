4af3e00647b01947db9161e5183984eaadb9dead
Abel Tadesse <tadesseabel26@gmail.com>
2025-06-04 21:52:26 +0300

    feat: implement setup page and initial profile logic, update Prisma schema for consistency

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation";
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+
+async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return <div>Create Server</div>;
+}
+
+export default SetupPage;
