6e06977dcb30016e7ba51beb0756208c75cb798a
Abel Tadesse <tadesseabel26@gmail.com>
2025-10-13 23:39:59 +0300

    feat: add server search component with channel and member filtering

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,13 +1,36 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@/lib/generated/prisma";
+import { ChannelType, MemberRole } from "@/lib/generated/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import ServerHeader from "./server-header";
+import {
+  Hash,
+  Mic,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  Video,
+} from "lucide-react";
+import { ScrollArea } from "../ui/scroll-area";
+import { ServerSearch } from "./server-search";
 
 interface serverSidebarProps {
   serverId: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 size-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 size-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 size-4" />,
+};
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="mr-2 size-4 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 size-4 text-indigo-500" />,
+};
 
 async function ServerSidebar({ serverId }: serverSidebarProps) {
   const profile = await currentProfile();
@@ -38,13 +61,13 @@ async function ServerSidebar({ serverId }: serverSidebarProps) {
 
   if (!server) return redirect("/");
 
-  const TextChannel = server?.channels.filter(
+  const textChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.TEXT
   );
-  const VideoChannel = server?.channels.filter(
+  const videoChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.VIDEO
   );
-  const AudioChannel = server?.channels.filter(
+  const audioChannels = server?.channels.filter(
     (channel) => channel.type === ChannelType.AUDIO
   );
   const members = server?.members.filter(
@@ -58,6 +81,50 @@ async function ServerSidebar({ serverId }: serverSidebarProps) {
   return (
     <div className="h-full w-full text-primary dark:bg-[#2B2D31] bg-[#F2F3F5] flex flex-col">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "members",
+                type: "member",
+                data: members?.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
